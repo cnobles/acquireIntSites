ec3cc3230a20d613f7d2cc40fa36b6dddff936b5
nobles <chris.l.nobles@gmail.com>
2015-07-22 15:34:56 -0400

    Working version with input of GTSP, patient, or csv with either info in a single column for multiple samples.

--- acquireIntSites.R
+++ acquireIntSites.R
@@ -15,27 +15,8 @@ gtspid <- args[1]
 query_by <- "GTSP"
 sample_query <- gtspid
 
-if(!grepl("GTSP", gtspid)){
-  sample_query <- gtspid
-  
-  junk <- sapply(dbListConnections(MySQL()), dbDisconnect) 
-  dbConn <- dbConnect(MySQL(), group="specimen_management")  
-  stopifnot(dbGetQuery(dbConn, "SELECT 1")==1)
-  
-  query_selection <- "SELECT Patient,SpecimenAccNum FROM specimen_management.gtsp "
-  string <- paste(sample_query, collapse="' OR Patient = '")
-  query_request <- paste0("WHERE Patient = '", string, "'")
-  query <- paste0(query_selection, query_request)
-  patient_GTSP <- dbGetQuery(dbConn, query)
-  
-  dbDisconnect(dbConn)
-  rm(string, query, query_request, query_selection, dbConn, junk)
-  
-  gtspid <- unique(patient_GTSP$SpecimenAccNum)
-  query_by <- "patient"
-
-}else if(grepl(".csv", gtspid)){
-  queryInfo <- as.data.frame(read.csv(file = grep(".csv", gtspid)))
+if(grepl(".csv", gtspid)){
+  queryInfo <- read.csv(file = gtspid)
   if("patient" %in% colnames(queryInfo) & "GTSP" %in% colnames(queryInfo)){
     stop("Both patient and GTSP columns found in ", gtspid,".\n\tPlease only use one.")
   }else if("patient" %in% colnames(queryInfo)){
@@ -63,6 +44,25 @@ if(!grepl("GTSP", gtspid)){
   }else{
     stop("Neither patient or GTSP column found in csv file.")
   }
+}else if(!grepl("GTSP", gtspid)){
+  sample_query <- gtspid
+  
+  junk <- sapply(dbListConnections(MySQL()), dbDisconnect) 
+  dbConn <- dbConnect(MySQL(), group="specimen_management")  
+  stopifnot(dbGetQuery(dbConn, "SELECT 1")==1)
+  
+  query_selection <- "SELECT Patient,SpecimenAccNum FROM specimen_management.gtsp "
+  string <- paste(sample_query, collapse="' OR Patient = '")
+  query_request <- paste0("WHERE Patient = '", string, "'")
+  query <- paste0(query_selection, query_request)
+  patient_GTSP <- dbGetQuery(dbConn, query)
+  
+  dbDisconnect(dbConn)
+  rm(string, query, query_request, query_selection, dbConn, junk)
+  
+  gtspid <- unique(patient_GTSP$SpecimenAccNum)
+  query_by <- "patient"
+  
 }
 
 if( is.na(gtspid) ) { 
