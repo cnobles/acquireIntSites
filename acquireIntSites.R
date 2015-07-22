9fbae5feabfaa5b4b2ab98148106b9aaca43b410
nobles <chris.l.nobles@gmail.com>
2015-07-22 14:39:47 -0400

    Changed the script to take multiple inputs or even just a patientID.

--- acquireIntSites.R
+++ acquireIntSites.R
@@ -14,8 +14,39 @@ args <- commandArgs(trailingOnly=TRUE)
 gtspid <- args[1] 
 ##gtspid <- "GTSP0568" 
 
+if(grepl(".csv", gtspid)){
+  queryInfo <- as.data.frame(read.csv(file = grep(".csv", gtspid)))
+  if("patient" %in% colnames(queryInfo) & "GTSP" %in% colnames(queryInfo)){
+    stop("Both patient and GTSP columns found in ", gtspid,".\n\tPlease only use one.")
+  }else if("patient" %in% colnames(queryInfo)){
+    sample_query <- unique(queryInfo$patient)
+    
+    junk <- sapply(dbListConnections(MySQL()), dbDisconnect) 
+    dbConn <- dbConnect(MySQL(), group="specimen_management")  
+    stopifnot(dbGetQuery(dbConn, "SELECT 1")==1)
+    
+    query_selection <- "SELECT Patient,SpecimenAccNum FROM specimen_management.gtsp "
+    string <- paste(sample_query, collapse="' OR Patient = '")
+    query_request <- paste0("WHERE Patient = '", string, "'")
+    query <- paste0(query_selection, query_request)
+    patient_GTSP <- dbGetQuery(dbConn, query)
+    
+    dbDisconnect(dbConn)
+    rm(string, query, query_request, query_selection, dbConn, junk)
+    
+    gtspid <- unique(patient_GTSP$SpecimenAccNum)
+    query_by <- "patient"
+  }else if("GTSP" %in% colnames(queryInfo)){
+    sample_query <- unique(queryInfo$GTSP)
+    gtspid <- unique(queryInfo$GTSP)
+    query_by <- "GTSP"
+  }else{
+    stop("Neither patient or GTSP column found in csv file.")
+  }
+}
+
 if( is.na(gtspid) ) { 
-   message("Usage:\n\tRscript acquireSites.R GTSP####")  
+   message("Usage:\n\tRscript acquireSites.R GTSP????\n\tor\n\tRscript acquireSites.R *.csv")
    q(status=1) 
 } 
 
@@ -28,23 +59,46 @@ stopifnot(file.info("~/.my.cnf")$mode == as.octmode("600"))
 ## ~/.my.cnf must be present 
 junk <- sapply(dbListConnections(MySQL()), dbDisconnect) 
 dbConn <- dbConnect(MySQL(), group="intSitesDev237")  
-stopifnot(dbGetQuery(dbConn, "SELECT 1")==1) 
+stopifnot(dbGetQuery(dbConn, "SELECT 1")==1)
 
 allSampleName <- suppressWarnings( dbGetQuery(dbConn, "SELECT * FROM samples") ) 
+dbDisconnect(dbConn)
+rm(dbConn, junk)
 
 ## get all replicates 
-replicates <- subset(allSampleName, grepl(paste0("^",gtspid), sampleName) ) 
+rep_list <- do.call(c, sapply(gtspid, function(x){
+  grep(x, allSampleName$sampleName, value = TRUE)
+  }))
+replicates <- allSampleName[match(rep_list, allSampleName$sampleName),] 
 stopifnot( nrow(replicates)>0 ) 
 
 ## get sampleInfo from specimen_management table
-sql <- sprintf("SELECT * FROM specimen_management.gtsp WHERE SpecimenAccNum = '%s'", gtspid) 
+junk <- sapply(dbListConnections(MySQL()), dbDisconnect) 
+dbConn <- dbConnect(MySQL(), group="specimen_management")  
+stopifnot(dbGetQuery(dbConn, "SELECT 1")==1) 
+
+string <- paste(gtspid, collapse="' OR SpecimenAccNum = '")
+query_request <- paste0("WHERE SpecimenAccNum = '", string, "'")
+sql <- paste0("SELECT * FROM specimen_management.gtsp ", query_request) 
 sampleInfo <- suppressWarnings( dbGetQuery(dbConn, sql) ) 
-stopifnot( nrow(sampleInfo)==1) 
+dbDisconnect(dbConn) #Disconnect from specimen_management after query
+rm(junk, dbConn, string, query_request) #Cleaning up so there is no interference or mistakes with the second query
+
+if(query_by == "patient"){
+  isThere <- unique(sampleInfo$Patient)
+}else if(query_by == "GTSP"){
+  isThere <- unique(sampleInfo$SpecimenAccNum)
+}
+stopifnot( (NA %in% match(sample_query, isThere)) == FALSE) 
 colnames(sampleInfo) <- tolower(colnames(sampleInfo)) 
 
 sampleIDin <- sprintf("(%s)", paste(unique(replicates$sampleID), collapse=",")) 
 
 ##get unique sites 
+junk <- sapply(dbListConnections(MySQL()), dbDisconnect) 
+dbConn <- dbConnect(MySQL(), group="intSitesDev237")  
+stopifnot(dbGetQuery(dbConn, "SELECT 1")==1) 
+
 sql <- paste("SELECT DISTINCT * 
              FROM samples JOIN sites 
              ON samples.sampleID = sites.sampleID 
@@ -65,32 +119,12 @@ sites.multi <- suppressWarnings( dbGetQuery(dbConn, sql) )
 sites.multi <- sites.multi[, !duplicated(colnames(sites.multi))] 
 sites.multi$breakpoint <- ifelse(sites.multi$strand=="+", 
 sites.multi$position+sites.multi$length-1, 
-sites.multi$position-sites.multi$length+1) 
+sites.multi$position-sites.multi$length+1)
 
-##output bed file 
-needed <- c("chr", "position", "breakpoint",
-            "sampleName", "refGenome", "strand") 
+dbDisconnect(dbConn)
+rm(dbConn, junk)
 
-bed <- rbind(subset(sites.uniq, select=needed), 
-             subset(sites.multi, select=needed)) 
-
-bed <- plyr::arrange(bed, chr, position, strand) 
-
-##bed$name <- gtspid 
-bed$name <- paste(sampleInfo$patient, sampleInfo$timepoint, sampleInfo$celltype, sep=":") 
-bed$score <- 400 
-
-bed <- plyr::arrange(bed, chr, position, strand) 
-
-bed <- data.frame(chrom=as.character(bed$chr), 
-                  chromStart=as.integer(pmin(bed$position, bed$breakpoint)), 
-                  chromEnd=as.integer(pmax(bed$position, bed$breakpoint)), 
-                  name=as.character(bed$name), 
-                  score=500, 
-                  strand=as.character(bed$strand)) 
-
-fileName <- paste(gtspid, sampleInfo$patient, sampleInfo$timepoint, sampleInfo$celltype, "bed", sep=".") 
-fileName <- paste(gtspid, "bed", sep=".") 
-
-write.table(bed, file=fileName, 
-            quote=FALSE, sep="\t", row.names=FALSE, col.names=FALSE) 
+#Save data of interest
+save(sampleInfo, file = "sampleInfo.RData")
+save(sites.uniq, file = "sites.uniq.RData")
+save(sites.multi, file = "sites.multi.RData")
